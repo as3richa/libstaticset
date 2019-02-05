ee8fa19968405cb9f5e23dcfb7c3b48d6fd6dbb3
Adam Richardson <as3richa@uwaterloo.ca>
2019-02-04 21:31:10 -0500

    Improve lowerBound specs

--- spec/search-spec.cpp
+++ spec/search-spec.cpp
@@ -4,8 +4,9 @@
 #include <iostream>
 #include <random>
 
+std::default_random_engine generator;
+
 std::vector<int> generateRandomVector(size_t count) {
-  std::default_random_engine generator;
   std::uniform_int_distribution<int> distribution(-(1 << 16), (1 << 16));
 
   std::vector<int> data;
@@ -22,9 +23,16 @@ std::vector<int> generateRandomVector(size_t count) {
 
 describe("search", []() {
   describe("lowerBound", []() {
-    it("yields an iterator pointing at the smallest elem. GTE the query, or end() if no such element exists", []() {
-      const size_t n_cases = 20;
-      const size_t sizes[n_cases] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 100, 1000, 10000};
+    it("yields an iterator pointing at the smallest elem. GTE the query, or end() if no such elem. exists", []() {
+      const size_t n_cases = 260;
+      size_t sizes[n_cases];
+
+      for(size_t i = 0; i <= 256; i ++) {
+        sizes[i] = i;
+      }
+      sizes[257] = 1000;
+      sizes[258] = 10000;
+      sizes[259] = 100000;
 
       for (size_t i = 0; i < n_cases; i++) {
         std::vector<int> data = generateRandomVector(sizes[i]);
@@ -33,16 +41,26 @@ describe("search", []() {
         int prev = INT_MIN;
 
         for (auto value : data) {
-          std::default_random_engine generator;
           std::uniform_int_distribution<int> distribution(prev + 1, value);
 
+          /* Try 10 random queries strictly greater than the next smallest value */
           for (int k = 0; k < 10; k++) {
             const int query = distribution(generator);
             expect(*ss.lowerBound(query) == value);
           }
+
+          /* Try the value itself */
           expect(*ss.lowerBound(value) == value);
 
           prev = value;
+        }
+
+        std::uniform_int_distribution<int> distribution(prev + 1, INT_MAX);
+
+        /* Try 10 random queries strictly greater than the maximum element of the set */
+        for(int k = 0; k < 10; k ++) {
+          const int query = distribution(generator);
+          expect(ss.lowerBound(query) == ss.end());
         }
       }
     });
